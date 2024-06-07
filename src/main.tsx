4e83308ae4f6b21ffe99f33c1adf00fe4cb833c9
FedotovFrontalex <permmedium@yandex.ru>
2024-06-07 13:24:04 +0500

    aff telegram script

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,13 @@
-//import React from 'react'
-//import ReactDOM from 'react-dom/client'
-//import App from './App.tsx'
-//import './index.css'
-//import {BrowserRouter} from "react-router-dom";
-//
-//ReactDOM.createRoot(document.getElementById('root')!).render(
-//  <React.StrictMode>
-//    <BrowserRouter>
-//      <App />
-//    </BrowserRouter>
-//  </React.StrictMode>,
-//)
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import {BrowserRouter} from "react-router-dom";
 
-import {Telegraf} from 'telegraf';
-
-const token = '6480149527:AAHWvvo8FY1herbqaAnAMfKzgri-NpHdwyA';
-const bot = new Telegraf(token);
-
-bot.command('start', (ctx) => {
-	ctx.reply('Hello')
-})
-
-bot.launch();
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
